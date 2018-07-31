018268e9ba10d149593c840e2b0872a1eaecda69
Jaret Screws <jscrews@pomeroy.com>
2018-07-31 12:43:43 -0400

    testing how difflib works

--- tagger.py
+++ tagger.py
@@ -1,3 +1,15 @@
 import difflib
-import sys
 
+with open('input/test.txt', 'r') as inputFile:
+  with open('output/test.txt', 'r') as outputFile:
+    diff = difflib.unified_diff(
+      outputFile.readlines(),
+      inputFile.readlines(),
+      fromfile='outputFile',
+      tofile='inputFile'
+    )
+    for line in diff:
+      if line[0:3] == "+++" or line[0:3] == "---" or line[0:2] == "@@":
+        print(line)
+"""       if line[0] is '+':
+        print(line) """
